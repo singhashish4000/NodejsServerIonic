674a4a8482ede94360b1b92a00160c7f100a3a11
Ashish Singh <singhashish4000@gmail.com>
2018-09-02 00:40:11 +0530

    Corected db url

--- src/server-router.ts
+++ src/server-router.ts
@@ -10,8 +10,9 @@ const serverRouter = express.Router();
 
 // CORS
 serverRouter.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	next();
 });
 /**
