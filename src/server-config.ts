17f471e27beee95ccad7700dfbee2baf45a160a3
Ashish Singh <singhashish4000@gmail.com>
2018-09-02 01:03:03 +0530

    Corected db url

--- src/server-config.ts
+++ src/server-config.ts
@@ -8,8 +8,8 @@ const serverConfig = {
 		host: 'https://node-server-for-ionic.herokuapp.com' || 'localhost',
 		port: 5432,
 		database: 'chat-app',
-		// user: 'postgres',
-		// password: 'postgres',
+		user: 'uviomwpzyxedbg',
+		password: '1ef62dfbd85c24c8e84c47b3cb5eae2e81c910b683a5fe7932feabd4c5e502fd',
 		max: 10, // maksymalna liczba połączeń do bazy danych
 		idleTimeoutMillis: 30000
 	},
