fe74bbe6de763b5ecaf37b400c513a0d1cbda6ef
Ashish Singh <singhashish4000@gmail.com>
2018-09-04 16:01:48 +0530

    hard coded for testing

--- src/server-router.ts
+++ src/server-router.ts
@@ -109,8 +109,8 @@ serverRouter.get('/get-all-messages', (req, res) => {
 			src_userId: req.get('src_userId'),
 			dest_userId: req.get('dest_userId')
 		}).subscribe(value => {
-			res.json({ status: 0, message: value });
 			console.log(res.json({ status: 0, message: value }));
+			res.json({ status: 0, message: value });
 		}, (error: Error) => {
 			res.json({ status: (-1), message: error.message });
 		});
