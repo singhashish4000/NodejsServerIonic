edd0470e77bd93a2561c037f3715eaaa7bc72922
Ashish Singh <singhashish4000@gmail.com>
2018-09-05 15:55:08 +0530

    Added names to response of all message api

--- src/data-model.ts
+++ src/data-model.ts
@@ -32,9 +32,25 @@ const dataModelUsers = {
 		return Observable.create((observer: Subscriber<any>) => {
 			pool.connect().then(client => {
 				let results = [];
+				let first_username;
+				let second_username;
+				let stmt;
 				client.query('SELECT * FROM messages WHERE m_user_id_one = 6  AND m_user_id_two = 5;').then(result => {
-					result.rows.forEach(row => {
-						let stmt = { type: 'private-message', time: row.m_data, login: 'S', text: row.m_content } 
+				client.query('SELECT user_login FROM users WHERE  user_id = 6').then(result => {
+                             first_username = result
+				});
+				client.query('SELECT user_login FROM users WHERE  user_id = 5').then(result => {
+					         second_username = result
+				});
+				
+							result.rows.forEach(row => {
+								if (row.m_user_id_one == 6 ) {
+									stmt = { type: 'private-message', time: row.m_data, login: first_username, text: row.m_content }
+								}
+								if (row.m_user_id_two == 7 ) {
+									stmt = { type: 'private-message', time: row.m_data, login: second_username, text: row.m_content }
+								}
+
 						    console.log(stmt);
 						    console.log(row);
 							results.push(stmt);
