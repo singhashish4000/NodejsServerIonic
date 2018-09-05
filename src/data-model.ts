061987dab430f5ce7bcb6f578ee13527b00dd7d3
Ashish Singh <singhashish4000@gmail.com>
2018-09-05 16:23:10 +0530

    Added names to response of all message api

--- src/data-model.ts
+++ src/data-model.ts
@@ -35,30 +35,28 @@ const dataModelUsers = {
 				let first_username = '';
 				let second_username = '';
 				let stmt;
-
-
+				client.query('SELECT * FROM users WHERE  user_id = 6').then(result => {
+					result.rows.forEach(row => {
+						first_username = row.user_login
+						console.log(first_username);
+					});		
+				});
+				client.query('SELECT * FROM users WHERE  user_id = 5').then(result => {
+					result.rows.forEach(row => {
+						second_username = row.user_login
+						console.log(second_username);
+					});		
+				});
 				client.query('SELECT * FROM messages WHERE m_user_id_one = 6  AND m_user_id_two = 5;').then(result => {
 							result.rows.forEach(row => {
 								if (row.m_user_id_one == 6 ) {
-									client.query('SELECT * FROM users WHERE  user_id = 6').then(result => {
-										result.rows.forEach(row => {
-											first_username = row.user_login
-											console.log(first_username);
-											stmt = { type: 'private-message', time: row.m_data, login: first_username, text: row.m_content }
-										});		
-									});
+									stmt = { type: 'private-message', time: row.m_data, login: first_username, text: row.m_content }
 								}
 								else if (row.m_user_id_one == 5 ) {
-									client.query('SELECT * FROM users WHERE  user_id = 5').then(result => {
-										result.rows.forEach(row => {
-											second_username = row.user_login
-											console.log(second_username);
-											stmt = { type: 'private-message', time: row.m_data, login: second_username, text: row.m_content }
-										});		
-									});
-							
+									stmt = { type: 'private-message', time: row.m_data, login: second_username, text: row.m_content }
 								}
 						    console.log(stmt);
+						    console.log(row);
 							results.push(stmt);
 					});
 					if (results.length > 1) {
